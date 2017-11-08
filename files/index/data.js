600bd603752da3644068c7cdcd78ff07d1be8ea5
limumu1992 <Chauncey_Lee@icloud.com>
2017-11-08 15:50:52 +0800

    微信4tab切换

---
+++ files/index/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n],o,_(p,q,r,s,t,u,v,_(),w,_(x,y,z,A,B,_(C,D,E,F),G,null,H,A,I,A,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(),U,_(V,_(W,X,Y,[_(W,Z,ba,g,bb,[_(bc,bd,W,be,bf,_(bg,o,b,bh,bi,bj),bk,bl)])])),bm,_(bn,[])),bo,_(),bp,_());}; 
+var b="url",c="index.html",d="generationDate",e=new Date(1510127422175.7),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="smsrandom",o="page",p="packageId",q="09b2184f90264e23b1b2318670c1a556",r="type",s="Axure:Page",t="name",u="index",v="notes",w="style",x="baseStyle",y="627587b6038d43cca051c114ac41ad32",z="pageAlignment",A="near",B="fill",C="fillType",D="solid",E="color",F=0xFFD7D7D7,G="image",H="imageHorizontalAlignment",I="imageVerticalAlignment",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="interactionMap",V="onLoad",W="description",X="页面载入时",Y="cases",Z="Case 1",ba="isNewIfGroup",bb="actions",bc="action",bd="linkWindow",be="在 当前窗口 打开 message",bf="target",bg="targetType",bh="message.html",bi="includeVariables",bj=true,bk="linkType",bl="current",bm="diagram",bn="objects",bo="masters",bp="objectPaths";
+return _creator();
+})());
